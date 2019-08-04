92d100e6960a42c2297e9234faac60e6c4698857
Ben Braunstein <benzbraunstein@gmail.com>
2019-08-04 00:15:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+const leastFavoriteCustomer = "ben";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(s) {
+  bestCustomer = s;
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Yosef";
+}
